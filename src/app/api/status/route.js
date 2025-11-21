5ab8c7d33922cf39c8a848b03429434ce05252a0
Danilov_Nikolai <dan1k91@mail.ru>
2025-11-21 19:25:25 +0300

    chore: comment smth to test result

--- src/app/api/status/route.js
+++ src/app/api/status/route.js
@@ -1,48 +1,48 @@
-// app/api/status/route.js
-import { NextResponse } from 'next/server';
+// // app/api/status/route.js
+// import { NextResponse } from 'next/server';
 
-export async function GET(req) {
-  try {
-    const { searchParams } = new URL(req.url);
-    const jobId = searchParams.get('jobId');
+// export async function GET(req) {
+//   try {
+//     const { searchParams } = new URL(req.url);
+//     const jobId = searchParams.get('jobId');
 
-    if (!jobId) {
-      return NextResponse.json(
-        {
-          success: false,
-          error: 'Не передан jobId',
-        },
-        { status: 400 }
-      );
-    }
+//     if (!jobId) {
+//       return NextResponse.json(
+//         {
+//           success: false,
+//           error: 'Не передан jobId',
+//         },
+//         { status: 400 }
+//       );
+//     }
 
-    const backendRes = await fetch(`${process.env.SERVER_API_URL}/parse/${jobId}/status`, {
-      method: 'GET',
-    });
+//     const backendRes = await fetch(`${process.env.SERVER_API_URL}/parse/${jobId}/status`, {
+//       method: 'GET',
+//     });
 
-    let data;
-    try {
-      data = await backendRes.json();
-    } catch (e) {
-      console.error('Ошибка парсинга JSON от backend /parse/:jobId/status:', e);
-      return NextResponse.json(
-        {
-          success: false,
-          error: 'Ошибка получения статуса задачи',
-        },
-        { status: 500 }
-      );
-    }
+//     let data;
+//     try {
+//       data = await backendRes.json();
+//     } catch (e) {
+//       console.error('Ошибка парсинга JSON от backend /parse/:jobId/status:', e);
+//       return NextResponse.json(
+//         {
+//           success: false,
+//           error: 'Ошибка получения статуса задачи',
+//         },
+//         { status: 500 }
+//       );
+//     }
 
-    return NextResponse.json(data, { status: 200 });
-  } catch (err) {
-    console.error('Ошибка /api/status:', err);
-    return NextResponse.json(
-      {
-        success: false,
-        error: err.message || 'Неизвестная ошибка /api/status',
-      },
-      { status: 500 }
-    );
-  }
-}
+//     return NextResponse.json(data, { status: 200 });
+//   } catch (err) {
+//     console.error('Ошибка /api/status:', err);
+//     return NextResponse.json(
+//       {
+//         success: false,
+//         error: err.message || 'Неизвестная ошибка /api/status',
+//       },
+//       { status: 500 }
+//     );
+//   }
+// }
